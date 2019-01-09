12425878a84f4c62abd63ac1e3d5412e9f459d24
longbowliu <llb_feng@163.com>
2019-01-09 13:13:47 +0800

    base

--- ros_arduino_python/src/ros_arduino_python/base_controller.py
+++ ros_arduino_python/src/ros_arduino_python/base_controller.py
@@ -169,7 +169,7 @@ class BaseController:
                 (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
                 rospy.Time.now(),
                 self.base_frame,
-                "robo1_tf/odom"
+                "robo1/odom"
                 )
     
             odom = Odometry()
